c8e442fd388da6de3b3d1b93cc8c2380d4ac50f9
Luispaguti <l.palaciosgutierrez@gmail.com>
2023-03-25 20:21:31 +0100

    photos

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../portfolio/portfolio.css'
-import IMG1 from '../../assets/portfolio1.jpg'
+import IMG1 from '../../assets/IMG1.png'
 import IMG2 from '../../assets/portfolio2.jpg'
 import IMG3 from '../../assets/portfolio3.jpg'
 import IMG4 from '../../assets/portfolio4.jpg'
@@ -17,9 +17,9 @@ function Portfolio() {
           <div className="portfolio_item-image"></div>
 
           <img src={IMG1} alt=""/>
-          <h3>This is a portfolio item title</h3>
-          <a href='https://github.com/Luispaguti' className='btn'>Github</a>
-          <a href='https://proyect-production.up.railway.app/login' className='btn btn-primary' target='_blank'>Live Demo</a>
+          <h3>Responsive Website with Modern UI/UX in React JS + Vite and Tailwind</h3>
+          <a href='https://github.com/Luispaguti/Blackside' className='btn'>Github</a>
+          <a href='https://blackside.es/' className='btn btn-primary' target='_blank'>Try it!</a>
         </article>
         <article className='portfolio__item'>
           <div className="portfolio_item-image"></div>
